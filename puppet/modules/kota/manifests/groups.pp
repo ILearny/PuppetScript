5e2a026fcbf8a19a00ad7e6b44048799c217ebd7
Vijaya Venu <sandeshjkota@gmail.com>
2018-07-24 19:34:13 +0530

    Update groups.pp

--- puppet/modules/kota/manifests/groups.pp
+++ puppet/modules/kota/manifests/groups.pp
@@ -1,1 +1,33 @@
 
+class kota::groups ($deployment_users, $batch_users, $iis_users) {
+
+	# ------------------------------
+	# Set all users
+	# ------------------------------
+
+	$admin_users_Deployment = $deployment_users
+	$admin_users_Batch = $batch_users
+	$admin_users_IIS = $iis_users
+
+
+	# -----------------------------------------	
+	# Add users to 'Administrators' group
+	# -----------------------------------------
+
+	if ($::asset == 'FRONT') {
+		$administrator_members_array = concat($admin_users_Deployment, $admin_users_IIS)
+	}
+	elsif ($::asset == 'BATCH') {
+		$administrator_members_array = concat($admin_users_Deployment, $admin_users_Batch)
+	}
+	elsif ($::asset == 'ALL') {
+		$administrator_members_array = concat($admin_users_Deployment, $admin_users_Batch, $admin_users_IIS)
+	}
+
+	group { 'Admin':
+		name => 'Administrators',
+		ensure   => present,
+		auth_membership => false,
+		members  => [$administrator_members_array],
+	}
+}
