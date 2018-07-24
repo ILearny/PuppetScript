fdd265c3f31eccf64219c7d2e58b5c795ac76120
Vijaya Venu <sandeshjkota@gmail.com>
2018-07-24 19:41:46 +0530

    Update softwares.pp

--- puppet/modules/kota/manifests/softwares.pp
+++ puppet/modules/kota/manifests/softwares.pp
@@ -1,1 +1,26 @@
 
+class kota::softwares ( $environment, $sourcepath, $destinationpath ) {
+	
+	# ------------------------------
+	# Set ressources path
+	# ------------------------------
+	$soft_source_path = $sourcepath
+	$res_dest = $destinationpath
+
+
+	# ------------------------------
+	# Set all softwares to install
+	# ------------------------------
+		
+	$soft_npp = 'npp-6.5.3.exe'
+	
+	# -----------------------------------------	
+	# Add softwares 
+	# -----------------------------------------
+
+	exec { $soft_npp:
+		command => "$soft_source_path\\$soft_npp /S",
+		path    => ['C:\Windows\System32'],
+	}
+}
+
